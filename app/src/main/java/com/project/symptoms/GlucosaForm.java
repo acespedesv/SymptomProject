86b75648818c9fe134e8ad814a87170c8d85cef5
isaac.mena07 <xsaco07@gmail.com>
2020-03-18 15:11:16 -0600

    Add main UI to presion form

---
+++ app/src/main/java/com/project/symptoms/GlucosaForm.java
@@ -0,0 +1,24 @@
+package com.project.symptoms;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class GlucosaForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_glucosa_form);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
