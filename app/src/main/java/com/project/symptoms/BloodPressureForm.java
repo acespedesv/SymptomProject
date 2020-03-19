1665e8e36d2d71abe6f0d47fb1b91121925f4e72
isaac.mena07 <xsaco07@gmail.com>
2020-03-19 11:31:25 -0600

    Add sample gradient

--- app/src/main/java/com/project/symptoms/BloodPressureForm.java
+++ app/src/main/java/com/project/symptoms/BloodPressureForm.java
@@ -1,19 +1,49 @@
 package com.project.symptoms;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class BloodPressureForm extends AppCompatActivity {
 
+    protected Toolbar toolbar;
+    protected Button saveButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blood_pressure_form);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        init();
+
+    }
+
+    private void init(){
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        saveButton = findViewById(R.id.save_button);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveData();
+            }
+        });
+    }
+
+    private void saveData(){
+
+        // TODO: Save data here
+
+        Intent mainActivityIntent = new Intent(this, MainActivity.class);
+        String text = "Data registered";
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(getApplicationContext(), text, duration).show();
+        startActivity(mainActivityIntent);
     }
 
 }
