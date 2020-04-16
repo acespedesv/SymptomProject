e5a74d3f33836aa76b3e335f0ba6f27acb61a39e
danblanco <blancodaniel2010@hotmail.com>
2020-04-15 21:09:14 -0600

    se agregan los icono nuevos

--- app/src/main/java/com/project/symptoms/SymptomForm.java
+++ app/src/main/java/com/project/symptoms/SymptomForm.java
@@ -27,102 +27,102 @@ public class SymptomForm extends AppCompatActivity {
         optionsList = new HashMap<>();
 
         SymptomOption dolores[] = {
-                new SymptomOption(R.drawable.ic_launcher_background, "Agudo"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Punzante"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Palpitante"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Opresivo"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Eléctrico")
+                new SymptomOption(R.drawable.ic_agudo, "Agudo"),
+                new SymptomOption(R.drawable.ic_punzante, "Punzante"),
+                new SymptomOption(R.drawable.ic_palpitante, "Palpitante"),
+                new SymptomOption(R.drawable.ic_opresivo, "Opresivo"),
+                new SymptomOption(R.drawable.ic_electrico, "Eléctrico")
         };
         optionsList.put("Tipo de dolor", dolores);
 
         SymptomOption digestivos[] = {
-                new SymptomOption(R.drawable.ic_launcher_background, "Nauseas"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Mal aliento"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Agruras"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Acidez"),
+                new SymptomOption(R.drawable.ic_nauseas, "Nauseas"),
+                new SymptomOption(R.drawable.ic_mal_aliento, "Mal aliento"),
+                new SymptomOption(R.drawable.ic_agruras, "Agruras"),
+                new SymptomOption(R.drawable.ic_acidez, "Acidez"),
                 new SymptomOption(R.drawable.ic_reflujo, "Reflujo"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Inflamación"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Estreñimiento"),
+                new SymptomOption(R.drawable.ic_inflamacion, "Inflamación"),
+                new SymptomOption(R.drawable.ic_estrenimiento, "Estreñimiento"),
                 new SymptomOption(R.drawable.ic_diarrea, "Diarrea"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Vómito"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Dificultad para tragar"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Sangrado"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Incotinencia"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Úlceras bucales")
+                new SymptomOption(R.drawable.ic_vomito, "Vómito"),
+                new SymptomOption(R.drawable.ic_dificultad_para_tragar, "Dificultad para tragar"),
+                new SymptomOption(R.drawable.ic_sangrad, "Sangrado"),
+                new SymptomOption(R.drawable.ic_incontinencia, "Incotinencia"),
+                new SymptomOption(R.drawable.ic_ulceras_bucales, "Úlceras bucales")
         };
         optionsList.put("Digestivo", digestivos);
 
         SymptomOption respiratorio[] = {
-                new SymptomOption(R.drawable.ic_launcher_background, "Falta de aire"),
+                new SymptomOption(R.drawable.ic_falta_de_aire, "Falta de aire"),
                 new SymptomOption(R.drawable.ic_catarro, "Catarro"),
                 new SymptomOption(R.drawable.ic_tos, "Tos"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Flema"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Congestión"),
+                new SymptomOption(R.drawable.ic_flemas, "Flema"),
+                new SymptomOption(R.drawable.ic_congestion, "Congestión"),
                 new SymptomOption(R.drawable.ic_irritacion, "Irritación"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Silbido")
+                new SymptomOption(R.drawable.ic_silbido, "Silbido")
         };
         optionsList.put("Respiratorio", respiratorio);
 
 
         SymptomOption sensorial[] = {
-                new SymptomOption(R.drawable.ic_launcher_background, "Mareo"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Alteraciones Visuales"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Alteraciones Auditivas"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Hormigueo"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Adormecimiento"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Rigidez"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Pesadez"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Debilidad")
+                new SymptomOption(R.drawable.ic_mareos, "Mareo"),
+                new SymptomOption(R.drawable.ic_alteraciones_visuales, "Alteraciones Visuales"),
+                new SymptomOption(R.drawable.ic_alteraciones_auditivas, "Alteraciones Auditivas"),
+                new SymptomOption(R.drawable.ic_hormigueo, "Hormigueo"),
+                new SymptomOption(R.drawable.ic_adormecimiento, "Adormecimiento"),
+                new SymptomOption(R.drawable.ic_rigidez, "Rigidez"),
+                new SymptomOption(R.drawable.ic_pesadez, "Pesadez"),
+                new SymptomOption(R.drawable.ic_debilidad, "Debilidad")
         };
         optionsList.put("Sensorial", sensorial);
 
 
         SymptomOption nervioso[] = {
-                new SymptomOption(R.drawable.ic_launcher_background, "Ansiedad"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Depresión"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Insomnio"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Somnolencia"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Cambios de humor"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Agotamiento")
+                new SymptomOption(R.drawable.ic_ansiedad, "Ansiedad"),
+                new SymptomOption(R.drawable.ic_depresion, "Depresión"),
+                new SymptomOption(R.drawable.ic_insomnio, "Insomnio"),
+                new SymptomOption(R.drawable.ic_somnolencia, "Somnolencia"),
+                new SymptomOption(R.drawable.ic_cambios_de_humor, "Cambios de humor"),
+                new SymptomOption(R.drawable.ic_agotamiento, "Agotamiento")
         };
         optionsList.put("Nervioso", nervioso);
 
         SymptomOption dermatologico[] = {
-                new SymptomOption(R.drawable.ic_launcher_background, "Hematomas"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Sarpullido/Erupción"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Manchas"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Enrojecimiento"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Acné"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Caida del cabello"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Úlceras"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Bultos en la piel"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Urticaria"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Comezón"),
+                new SymptomOption(R.drawable.ic_hematoma, "Hematomas"),
+                new SymptomOption(R.drawable.ic_sarpullido, "Sarpullido/Erupción"),
+                new SymptomOption(R.drawable.ic_manchas, "Manchas"),
+                new SymptomOption(R.drawable.ic_enrojecimiento, "Enrojecimiento"),
+                new SymptomOption(R.drawable.ic_acne, "Acné"),
+                new SymptomOption(R.drawable.ic_caida_de_cabello, "Caida del cabello"),
+                new SymptomOption(R.drawable.ic_ulceras, "Úlceras"),
+                new SymptomOption(R.drawable.ic_bultos_en_la_piel, "Bultos en la piel"),
+                new SymptomOption(R.drawable.ic_urticaria, "Urticaria"),
+                new SymptomOption(R.drawable.ic_comeson, "Comezón"),
         };
         optionsList.put("Dermatológico", dermatologico);
 
         SymptomOption actividadesDetonantes[] = {
                 new SymptomOption(R.drawable.ic_despertar, "Al despertar"),
                 new SymptomOption(R.drawable.ic_dormir, "Al dormir"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Durante actividad física"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Post actividad física")
+                new SymptomOption(R.drawable.ic_durante_actividad_fisica, "Durante actividad física"),
+                new SymptomOption(R.drawable.ic_despues_de_actividad, "Post actividad física")
         };
         optionsList.put("Actividades Detonantes", actividadesDetonantes);
 
         SymptomOption emocionesDetonantes[] = {
-                new SymptomOption(R.drawable.ic_launcher_background, "Susto"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Enojo"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Tristeza"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Estrés"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Angustia")
+                new SymptomOption(R.drawable.ic_susto, "Susto"),
+                new SymptomOption(R.drawable.ic_enojo, "Enojo"),
+                new SymptomOption(R.drawable.ic_tristeza, "Tristeza"),
+                new SymptomOption(R.drawable.ic_estres, "Estrés"),
+                new SymptomOption(R.drawable.ic_angustia, "Angustia")
         };
         optionsList.put("Dermatológico", emocionesDetonantes);
 
         SymptomOption sensacionClimatica[] = {
                 new SymptomOption(R.drawable.ic_frio, "Frio"),
                 new SymptomOption(R.drawable.ic_calor, "Calor"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Humedad"),
-                new SymptomOption(R.drawable.ic_launcher_background, "Sequedad")
+                new SymptomOption(R.drawable.ic_humedad, "Humedad"),
+                new SymptomOption(R.drawable.ic_sequedad, "Sequedad")
         };
         optionsList.put("Clima", sensacionClimatica);
 
