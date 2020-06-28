334fbe8484a96fb3d002f9fe3e9360117e58ac4c
xsaco07 <xsaco07@gmail.com>
2020-06-28 00:45:52 -0600

    Insert symptom category in DB

--- app/src/main/java/com/project/symptoms/db/controller/SymptomCategoryController.java
+++ app/src/main/java/com/project/symptoms/db/controller/SymptomCategoryController.java
@@ -2,6 +2,7 @@ package com.project.symptoms.db.controller;
 
 import android.content.Context;
 
+import com.project.symptoms.R;
 import com.project.symptoms.db.dao.SymptomCategoryDaoImpl;
 import com.project.symptoms.db.model.SymptomCategoryModel;
 
@@ -9,8 +10,10 @@ public class SymptomCategoryController {
 
     private static SymptomCategoryController instance;
     private SymptomCategoryDaoImpl symptomCategoryDao;
+    private Context context;
 
     private SymptomCategoryController(Context context){
+        this.context = context;
         symptomCategoryDao = new SymptomCategoryDaoImpl(context);
     }
 
@@ -24,9 +27,24 @@ public class SymptomCategoryController {
         boolean success = true;
         SymptomCategoryModel symptomCategoryModel = null;
         try{
-            symptomCategoryModel = new SymptomCategoryModel("Tipo de Dolor");
+            symptomCategoryModel = new SymptomCategoryModel(context.getResources().getString(R.string.pain_type));
             symptomCategoryDao.insert(symptomCategoryModel);
-
+            symptomCategoryModel = new SymptomCategoryModel(context.getResources().getString(R.string.digestive_pain));
+            symptomCategoryDao.insert(symptomCategoryModel);
+            symptomCategoryModel = new SymptomCategoryModel(context.getResources().getString(R.string.respiratory_pain));
+            symptomCategoryDao.insert(symptomCategoryModel);
+            symptomCategoryModel = new SymptomCategoryModel(context.getResources().getString(R.string.sensory_pain));
+            symptomCategoryDao.insert(symptomCategoryModel);
+            symptomCategoryModel = new SymptomCategoryModel(context.getResources().getString(R.string.emotional_pain));
+            symptomCategoryDao.insert(symptomCategoryModel);
+            symptomCategoryModel = new SymptomCategoryModel(context.getResources().getString(R.string.dermatological_pain));
+            symptomCategoryDao.insert(symptomCategoryModel);
+            symptomCategoryModel = new SymptomCategoryModel(context.getResources().getString(R.string.triggering_activity));
+            symptomCategoryDao.insert(symptomCategoryModel);
+            symptomCategoryModel = new SymptomCategoryModel(context.getResources().getString(R.string.triggering_feeling));
+            symptomCategoryDao.insert(symptomCategoryModel);
+            symptomCategoryModel = new SymptomCategoryModel(context.getResources().getString(R.string.triggering_weather_condition));
+            symptomCategoryDao.insert(symptomCategoryModel);
         }catch (Exception e){
             success = false;
             e.printStackTrace();
