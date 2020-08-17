f8b5dea70c3bff20489f3cda002218a71bdf826d
iamenalo <isaac.a.mena.lopez@intel.com>
2020-08-17 16:38:33 -0600

    Call login activity just once

--- app/src/main/java/com/project/symptoms/activity/MainActivity.java
+++ app/src/main/java/com/project/symptoms/activity/MainActivity.java
@@ -443,7 +443,7 @@ public class MainActivity extends AppCompatActivity implements
         if (requestCode == STORAGE_CODE) {
             // Permission was granted from popup, call generate pdf method
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                showDateRangeDialog();
+                checkLoginActivity();
             } else // Permission was denied from popup, show error message
                 Toast.makeText(this, getResources().getString(R.string.storage_permission_denied), Toast.LENGTH_SHORT).show();
         }
